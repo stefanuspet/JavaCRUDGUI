a851c20f7fee4bb45eebd517591d8f78e324c4ea
stefanuspet <stefanuspetra.p@gmail.com>
2023-06-03 23:00:51 +0700

    javaproject

---
+++ tubesObjectPresistence/src/connection/DbConnection.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author stefa
+ */
+public class DbConnection {
+    public static Connection CON;
+    public static final String URL = "jdbc:mysql://";
+    public static final String PATH = "localhost:3306/object-persistence";
+
+    public Connection makeConnection() {
+        System.out.println("Opening database...");
+        try {
+            CON = DriverManager.getConnection(URL + PATH, "root", "");
+            System.out.println("Succes !");
+        } catch (Exception e) {
+            System.out.println("Erorr opening database...");
+            System.out.println(e);
+        }
+        return CON;
+    }
+
+    public void closeConnection() {
+        System.out.println("Closing Database");
+        try {
+            CON.close();
+            System.out.println("Succes !");
+        } catch (Exception e) {
+            System.out.println("Erorr closing database...");
+            System.out.println(e);
+        }
+    }
+}
---
+++ tubesObjectPresistence/src/model/Penghuni.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author stefa
+ */
+public class Penghuni {
+    private int id_penghuni;
+    private String nama,alamat,no_telp,tanggal_masuk,tanggal_keluar;
+
+    public Penghuni(int id_penghuni, String nama, String alamat, String no_telp, String tanggal_masuk, String tanggal_keluar) {
+        this.id_penghuni = id_penghuni;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.no_telp = no_telp;
+        this.tanggal_masuk = tanggal_masuk;
+        this.tanggal_keluar = tanggal_keluar;
+    }
+
+    public int getId_penghuni() {
+        return id_penghuni;
+    }
+
+    public void setId_penghuni(int id_penghuni) {
+        this.id_penghuni = id_penghuni;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNo_telp() {
+        return no_telp;
+    }
+
+    public void setNo_telp(String no_telp) {
+        this.no_telp = no_telp;
+    }
+
+    public String getTanggal_masuk() {
+        return tanggal_masuk;
+    }
+
+    public void setTanggal_masuk(String tanggal_masuk) {
+        this.tanggal_masuk = tanggal_masuk;
+    }
+
+    public String getTanggal_keluar() {
+        return tanggal_keluar;
+    }
+
+    public void setTanggal_keluar(String tanggal_keluar) {
+        this.tanggal_keluar = tanggal_keluar;
+    }
+    
+    
+}
