15d1de4ef6617e014eac98b21be983c66345a45c
zeddvyy <vanayalerrick@gmail.com>
2023-06-12 00:19:21 +0700

    added exception

---
+++ tubesObjectPresistence/src/exception/InputUsernameSamaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author nayal
+ */
+public class InputUsernameSamaException extends Exception{
+    public String message(){
+        return "USERNAME PENGHUNI SUDAH ADA!";
+    }
+}
